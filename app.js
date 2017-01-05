639d019bdb14e4227f4837ac469c5965b1f74fb8
Joe Longstreet <joelongstreet@gmail.com>
2017-01-04 22:14:04 -0600

    use a layout engine

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ let express = require("express"),
     server = require('http').Server(app),
     io = require("socket.io")(server),
     gpio = require("./lib/gpio")(io),
-    port = process.env.PORT || 3000;
+    port = process.env.PORT || 3000,
+    categories = ["alphabet", "numbers", "quiz", "signs"];
 
 server.listen(port);
+
+app.set("view engine", "pug");
+app.set("views", "./public");
 
 app.use(express.static(
     path.join(__dirname, 'public')
@@ -18,3 +22,10 @@ app.use(express.static(
 app.get("/api/sounds", routes.sounds);
 app.get("/api/signs", routes.signs);
 app.get("/api/quiz", routes.quiz);
+app.get("/api/menu", (req, res) => res.send(categories));
+
+
+app.get("/", (req, res) => res.render("index"));
+categories.forEach(category => {
+    app.get(`/${category}`, (req, res) => res.render(`${category}`));
+});
