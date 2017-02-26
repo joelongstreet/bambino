bfb2ce2165596c6c14605bd594f03ec03be8384e
Joe Longstreet <joe.longstreet@c2fo.com>
2017-02-26 11:41:05 -0600

    clean up

--- public/lib/gpio.js
+++ public/lib/gpio.js
@@ -3,7 +3,6 @@ class Gpio{
         this.pinStatus = [0, 0, 0, 0];
         this.socket = io('/');
         this.emitter = new EventEmitter2();
-        this.throttled = false;
 
         document.addEventListener("keydown", e => {
             let index = (keyCode => {
@@ -22,19 +21,19 @@ class Gpio{
         });
 
         this.socket.on("gpio", d => {
-			if(this.pinStatus[d.pin] != d.val){
-				this.pinStatus[d.pin] = d.val;
+            if(this.pinStatus[d.pin] != d.val){
+                this.pinStatus[d.pin] = d.val;
 
-				if(this.pinStatus.every(el => el == 1)){
-					window.location.href = "/";
-				}
+                if(this.pinStatus.every(el => el == 1)){
+                    window.location.href = "/";
+                }
 
-				if(d.val == 1){
-					this.emitter.emit("input", d.pin);
-				}
+                if(d.val == 1){
+                    this.emitter.emit("input", d.pin);
+                }
 
-				this.emitter.emit("update", d.pin, d.val);
-			}
+                this.emitter.emit("update", d.pin, d.val);
+            }
         });
     }
 }
